52c42712df030848acb7f85f0987b2ec70e04a50
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-12-01 00:17:48 -0500

    Solve day 1

--- 1/p1.py
+++ 1/p1.py
@@ -2,3 +2,22 @@ from adventio import IStream
 
 cin = IStream()
 
+import sys
+sys.path.append("..")
+from adventio import IStream
+
+cin = IStream("in")
+
+lines = cin.all_lines()
+
+eats = []
+curr_sum = 0
+for line in lines:
+    if line.strip():
+        curr_sum += int(line)
+    else:
+        eats.append(curr_sum)
+        curr_sum = 0
+
+eats.sort()
+print(eats[-1])
---
+++ 1/p2.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.append("..")
+from adventio import IStream
+
+cin = IStream("in")
+
+lines = cin.all_lines()
+
+eats = []
+curr_sum = 0
+for line in lines:
+    if line.strip():
+        curr_sum += int(line)
+    else:
+        eats.append(curr_sum)
+        curr_sum = 0
+
+eats.sort()
+print(eats[-1] + eats[-2] + eats[-3])
