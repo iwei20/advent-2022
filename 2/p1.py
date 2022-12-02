0e66354007e10a1c489293935e2954e8b1d001ea
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-12-02 00:12:07 -0500

    Implement day 2

--- 2/p1.py
+++ 2/p1.py
@@ -1,5 +1,23 @@
 import sys
 sys.path.append("..")
 from adventio import IStream
+from collections import *
+from itertools import *
 
 cin = IStream("in")
+
+lines = cin.all_lines()
+
+scores = {
+    "A X": 1+3,
+    "B X": 1+0,
+    "C X": 1+6,
+    "A Y": 2+6,
+    "B Y": 2+3,
+    "C Y": 2+0,
+    "A Z": 3+0,
+    "B Z": 3+6,
+    "C Z": 3+3,
+}
+
+print(sum(scores[i] for i in lines))
--- 2/p2.py
+++ 2/p2.py
@@ -1,5 +1,23 @@
 import sys
 sys.path.append("..")
 from adventio import IStream
+from collections import *
+from itertools import *
 
 cin = IStream("in")
+
+lines = cin.all_lines()
+
+scores = {
+    "A X": 3+0,
+    "B X": 1+0,
+    "C X": 2+0,
+    "A Y": 1+3,
+    "B Y": 2+3,
+    "C Y": 3+3,
+    "A Z": 2+6,
+    "B Z": 3+6,
+    "C Z": 1+6,
+}
+
+print(sum(scores[i] for i in lines))
