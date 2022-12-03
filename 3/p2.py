d9bbe8630fdca3018ba547242b5115772db372dc
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-12-03 00:44:25 -0500

    Add a oneliner to part 2

--- 3/p2.py
+++ 3/p2.py
@@ -3,12 +3,13 @@ sys.path.append("..")
 from adventio import IStream
 from collections import *
 from itertools import *
+from more_itertools import *
 
 cin = IStream("in")
 
 lines = cin.all_lines()
 
-sum = 0
+s = 0
 for i in range(0, len(lines), 3):
     a = lines[i]
     b = lines[i + 1]
@@ -16,10 +17,12 @@ for i in range(0, len(lines), 3):
 
     for item in set(a).intersection(set(b)).intersection(set(c)):
         if (item.islower()):
-            sum += ord(item) - ord('a') + 1
+            s += ord(item) - ord('a') + 1
         else:
-            sum += ord(item) - ord('A') + 27
+            s += ord(item) - ord('A') + 27
 
-print(sum)
+print(s)
+
+print(sum(ord(set(a).intersection(set(b)).intersection(set(c)).pop()) - (ord('a') - 1 if set(a).intersection(set(b)).intersection(set(c)).pop().islower() else ord('A') - 27) for a, b, c in grouper(lines, 3)))
 
 
