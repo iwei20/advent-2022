b7ce5052824efb51e94efbb4e8a8b35bb96674c3
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-12-03 00:10:55 -0500

    Implement day 3

--- 3/p1.py
+++ 3/p1.py
@@ -1,5 +1,24 @@
 import sys
 sys.path.append("..")
 from adventio import IStream
+from collections import *
+from itertools import *
 
 cin = IStream("in")
+
+lines = cin.all_lines()
+
+sum = 0
+for line in lines:
+    firsthalf = line[:len(line)//2]
+    secondhalf = line[len(line)//2:]
+
+    for item in set(firsthalf).intersection(set(secondhalf)):
+        if (item.islower()):
+            sum += ord(item) - ord('a') + 1
+        else:
+            sum += ord(item) - ord('A') + 27
+
+print(sum)
+
+
--- 3/p2.py
+++ 3/p2.py
@@ -1,5 +1,25 @@
 import sys
 sys.path.append("..")
 from adventio import IStream
+from collections import *
+from itertools import *
 
 cin = IStream("in")
+
+lines = cin.all_lines()
+
+sum = 0
+for i in range(0, len(lines), 3):
+    a = lines[i]
+    b = lines[i + 1]
+    c = lines[i + 2]
+
+    for item in set(a).intersection(set(b)).intersection(set(c)):
+        if (item.islower()):
+            sum += ord(item) - ord('a') + 1
+        else:
+            sum += ord(item) - ord('A') + 27
+
+print(sum)
+
+
