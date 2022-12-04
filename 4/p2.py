6e2ac726fcf74b3ef065c656ca17b8fbe1dd1e9c
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-12-04 00:09:47 -0500

    Remove len from day4-p2

--- 4/p2.py
+++ 4/p2.py
@@ -14,7 +14,7 @@ pattern = compile("{a:d}-{b:d},{c:d}-{d:d}")
 count = 0
 for line in lines:
     result = pattern.parse(line)
-    if len(set(range(result["a"], result["b"] + 1)).intersection(set(range(result["c"], result["d"] + 1)))):
+    if set(range(result["a"], result["b"] + 1)).intersection(set(range(result["c"], result["d"] + 1))):
         count += 1
 
 print(count)
