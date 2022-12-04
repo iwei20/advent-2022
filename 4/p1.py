9347213902811880ec137ef5ca127cf888b2177c
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-12-04 00:09:17 -0500

    Implement day 4

--- 4/p1.py
+++ 4/p1.py
@@ -1,5 +1,22 @@
 import sys
 sys.path.append("..")
 from adventio import IStream
+from collections import *
+from itertools import *
+from more_itertools import *
+from parse import compile
 
 cin = IStream("in")
+
+lines = cin.all_lines()
+
+pattern = compile("{a:d}-{b:d},{c:d}-{d:d}")
+count = 0
+for line in lines:
+    result = pattern.parse(line)
+    ab = set(range(result["a"], result["b"] + 1))
+    cd = set(range(result["c"], result["d"] + 1))
+    if ab.issubset(cd) or cd.issubset(ab):
+        count += 1
+
+print(count)
--- 4/p2.py
+++ 4/p2.py
@@ -1,5 +1,20 @@
 import sys
 sys.path.append("..")
 from adventio import IStream
+from collections import *
+from itertools import *
+from more_itertools import *
+from parse import compile
 
 cin = IStream("in")
+
+lines = cin.all_lines()
+
+pattern = compile("{a:d}-{b:d},{c:d}-{d:d}")
+count = 0
+for line in lines:
+    result = pattern.parse(line)
+    if len(set(range(result["a"], result["b"] + 1)).intersection(set(range(result["c"], result["d"] + 1)))):
+        count += 1
+
+print(count)
