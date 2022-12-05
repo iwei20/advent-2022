b6c958110d999d46650e3ee80951639f2f287ecd
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-12-05 00:17:35 -0500

    Implement day 5

--- 5/p2.py
+++ 5/p2.py
@@ -1,5 +1,36 @@
 import sys
 sys.path.append("..")
 from adventio import IStream
+from collections import *
+from itertools import *
+from more_itertools import *
+from parse import compile
 
 cin = IStream("in")
+
+lines = cin.all_lines()
+lines = lines[10:]
+
+stacks = [
+    "BZT",
+    "VHTDN",
+    "BFMD",
+    "TJGWVQL",
+    "WDGPVFQM",
+    "VZQGHFS",
+    "ZSNRLTCW",
+    "ZHWDJNRM",
+    "MQLFDS",
+]
+
+stackslists = [list(string) for string in stacks]
+
+pattern = compile("move {:d} from {:d} to {:d}")
+
+for line in lines:
+    r = pattern.parse(line)
+    stackslists[r[2] - 1].extend(stackslists[r[1] - 1][-r[0]:])
+    for i in range(r[0]):
+        stackslists[r[1] - 1].pop()
+
+print(''.join([stacklist[-1] for stacklist in stackslists]))
