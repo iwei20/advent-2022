1d298ae39214ae8caf819b1adff764a05ef77693
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-12-06 00:07:28 -0500

    Finish day 6

--- 6/p2.py
+++ 6/p2.py
@@ -1,5 +1,17 @@
 import sys
 sys.path.append("..")
 from adventio import IStream
+from collections import *
+from itertools import *
+from more_itertools import *
+from parse import compile
 
 cin = IStream("in")
+
+lines = cin.all_lines()
+
+for line in lines:
+    for i, word in enumerate(windowed(line, 14)):
+        if len(set(word)) == 14:
+            print(i + 14)
+            break
