774a8b58471c920c6d1244caef17217cb78769ba
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-12-08 00:29:08 -0500

    Implement day 8

--- 8/p1.py
+++ 8/p1.py
@@ -1,5 +1,48 @@
 import sys
 sys.path.append("..")
 from adventio import IStream
+from collections import *
+from itertools import *
+from more_itertools import *
+from parse import compile
 
 cin = IStream("in")
+lines = cin.all_lines()
+
+treemap = [[int(c) for c in line] for line in lines]
+visible = [[False for _ in line] for line in lines]
+
+for r in range(len(treemap)):
+    visible[r][0] = True
+    visible[r][-1] = True
+    maxsofar = treemap[r][0]
+    for c in range(1, len(treemap[0])):
+
+        if maxsofar < treemap[r][c]:
+            visible[r][c] = True
+            maxsofar = treemap[r][c]
+    maxsofar = treemap[r][-1]
+    for c in range(len(treemap[0]) - 2, 0, -1):
+
+        if maxsofar < treemap[r][c]:
+            visible[r][c] = True
+            maxsofar = treemap[r][c]
+
+
+for c in range(len(treemap[0])):
+    visible[0][c] = True
+    visible[-1][c] = True
+    maxsofar = treemap[0][c]
+    for r in range(1, len(treemap)):
+        if maxsofar < treemap[r][c]:
+            visible[r][c] = True
+            maxsofar = treemap[r][c]
+    maxsofar = treemap[-1][c]
+    for r in range(len(treemap) - 2, 0, -1):
+
+        if maxsofar < treemap[r][c]:
+            visible[r][c] = True
+            maxsofar = treemap[r][c]
+
+print(visible)
+print(list(collapse(visible)).count(True))
--- 8/p2.py
+++ 8/p2.py
@@ -1,5 +1,48 @@
 import sys
 sys.path.append("..")
 from adventio import IStream
+from collections import *
+from itertools import *
+from more_itertools import *
+from parse import compile
 
 cin = IStream("in")
+lines = cin.all_lines()
+
+treemap = [[int(c) for c in line] for line in lines]
+visible = [[False for _ in line] for line in lines]
+
+scores = []
+for r in range(len(treemap)):
+    for c in range(len(treemap[0])):
+        score = 1
+
+        upscore = 0
+        rightscore = 0
+        leftscore = 0
+        downscore = 0
+        for up in range(r - 1, -1, -1):
+            upscore += 1
+            if (treemap[up][c] >= treemap[r][c]):
+                break
+        score *= upscore
+
+        for right in range(c + 1, len(treemap[0])):
+            rightscore += 1
+            if (treemap[r][right] >= treemap[r][c]):
+                break
+        score *= rightscore
+
+        for left in range(c - 1, -1, -1):
+            leftscore += 1
+            if (treemap[r][left] >= treemap[r][c]):
+                break
+        score *= leftscore
+
+        for down in range(r + 1, len(treemap)):
+            downscore += 1
+            if (treemap[down][c] >= treemap[r][c]):
+                break
+        score *= downscore
+        scores.append(score)
+print(max(scores))
