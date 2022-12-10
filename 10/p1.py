8f5d15b3379d9643343a06bedc57b19f2b8074e4
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-12-10 00:44:53 -0500

    Implement day 10

--- 10/p1.py
+++ 10/p1.py
@@ -1,5 +1,40 @@
 import sys
 sys.path.append("..")
 from adventio import IStream
+from collections import *
+from itertools import *
+from more_itertools import *
+from parse import compile
 
 cin = IStream("in")
+lines = iter(cin.all_lines())
+
+noop_pattern = compile("noop")
+addx_pattern = compile("addx {:d}")
+
+x = 1
+ans = 0
+addx_iter = 0
+
+curr = None
+rn = None
+ra = None
+
+cycle = 0
+while cycle < 220:
+    if ra is not None:
+        addx_iter += 1
+        if addx_iter == 2:
+            addx_iter = 0
+            x += ra[0]
+            curr = next(lines)
+    else:
+        curr = next(lines)
+    rn = noop_pattern.parse(curr)
+    ra = addx_pattern.parse(curr)
+
+    cycle += 1
+    if cycle in [20, 60, 100, 140, 180, 220]:
+        ans += x * cycle
+
+print(ans)
--- 10/p2.py
+++ 10/p2.py
@@ -1,5 +1,40 @@
 import sys
 sys.path.append("..")
 from adventio import IStream
+from collections import *
+from itertools import *
+from more_itertools import *
+from parse import compile
 
 cin = IStream("in")
+
+noop_pattern = compile("noop")
+addx_pattern = compile("addx {:d}")
+
+x = 1
+ans = [['.' for i in range(40)] for j in range(6)]
+addx_iter = 0
+
+curr = None
+rn = None
+ra = None
+
+cycle = 0
+while cycle < 240:
+    if ra is not None:
+        addx_iter += 1
+        if addx_iter == 2:
+            addx_iter = 0
+            x += ra[0]
+            curr = cin.next_line()
+    else:
+        curr = cin.next_line()
+    rn = noop_pattern.parse(curr)
+    ra = addx_pattern.parse(curr)
+    if cycle % 40 in [x - 1, x, x + 1]:
+        ans[cycle // 40][cycle % 40] = "#"
+    cycle += 1
+
+
+for row in ans:
+    print(''.join(row))
