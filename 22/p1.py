66815866136c6d44f77c6ee9b939b3ad369b7516
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-12-22 22:39:44 -0500

    Implement day 22

--- 22/p1.py
+++ 22/p1.py
@@ -1,5 +1,132 @@
 import sys
 sys.path.append("..")
 from adventio import IStream
+from collections import *
+from itertools import *
+from more_itertools import *
+from parse import compile
+import numpy as np
+import math
+import operator
 
 cin = IStream("in")
+
+max_len = 0
+maze_input = []
+
+lines = cin.all_lines()
+for line in lines[:-2]:
+    if max_len == 0:
+        maze_input.append(line)
+    else:
+        maze_input.append("".join(padded(line, " ", max_len)))
+    if len(line) > max_len:
+        max_len = len(line)
+        maze_input = ["".join(padded(item, " ", max_len)) for item in maze_input]
+
+space = set()
+walls = set()
+row_bounds = []
+for r, row in enumerate(maze_input):
+    start = None
+    end = None
+    for c, ele in enumerate(row):
+        if ele == '.':
+            if start is None:
+                start = c
+            space.add((r, c))
+        if ele == '#':
+            if start is None:
+                start = c
+            walls.add((r, c))
+        if ele == " " and start is not None and end is None:
+            end = c - 1
+    if end is None:
+        end = max_len - 1
+    row_bounds.append((start, end))
+
+col_bounds = []
+for c in range(len(maze_input[0])):
+    start = None
+    end = None
+    for r, row in enumerate(maze_input):
+        ele = row[c]
+        if ele == '.':
+            if start is None:
+                start = r
+        if ele == '#':
+            if start is None:
+                start = r
+        if ele == " " and start is not None and end is None:
+            end = r - 1
+    if end is None:
+        end = len(maze_input) - 1
+    col_bounds.append((start, end))
+
+cw = {
+    (0, 1): (1, 0),
+    (1, 0): (0, -1),
+    (0, -1): (-1, 0),
+    (-1, 0): (0, 1)
+}
+
+ccw = {
+    (0, 1): (-1, 0),
+    (-1, 0): (0, -1),
+    (0, -1): (1, 0),
+    (1, 0): (0, 1)
+}
+
+curr = (0, row_bounds[0][0])
+direction = (0, 1)
+instructions = lines[-1]
+curr_instruction = ""
+for char in instructions:
+    if char.isalpha():
+        # Walk
+        for i in range(int(curr_instruction)):
+            next_spot = (curr[0] + direction[0], curr[1] + direction[1])
+            if next_spot not in walls and next_spot not in space:
+                if direction == (0, 1):
+                    next_spot = (next_spot[0], row_bounds[next_spot[0]][0])
+                if direction == (0, -1):
+                    next_spot = (next_spot[0], row_bounds[next_spot[0]][1])
+                if direction == (1, 0):
+                    next_spot = (col_bounds[next_spot[1]][0], next_spot[1])
+                if direction == (-1, 0):
+                    next_spot = (col_bounds[next_spot[1]][1], next_spot[1])                                  
+            if next_spot in walls:
+                break
+            curr = next_spot
+        # Turn
+        if char == "L":
+            direction = ccw[direction]
+        if char == "R":
+            direction = cw[direction]
+        curr_instruction = ""
+    else:
+        curr_instruction += char
+# Walk
+for i in range(int(curr_instruction)):
+    next_spot = (curr[0] + direction[0], curr[1] + direction[1])
+    if next_spot not in walls and next_spot not in space:
+        if direction == (0, 1):
+            next_spot = (next_spot[0], row_bounds[next_spot[0]][0])
+        if direction == (0, -1):
+            next_spot = (next_spot[0], row_bounds[next_spot[0]][1])
+        if direction == (1, 0):
+            next_spot = (col_bounds[next_spot[1]][0], next_spot[1])
+        if direction == (-1, 0):
+            next_spot = (col_bounds[next_spot[1]][1], next_spot[1])                                  
+    if next_spot in walls:
+        break
+    curr = next_spot
+
+idx = {
+    (0, 1): 0,
+    (1, 0): 1,
+    (0, -1): 2,
+    (-1, 0): 3
+}
+
+print(1000 * (curr[0] + 1) + 4 * (curr[1] + 1) + idx[direction])
--- 22/p2.py
+++ 22/p2.py
@@ -1,5 +1,187 @@
 import sys
 sys.path.append("..")
 from adventio import IStream
+from collections import *
+from itertools import *
+from more_itertools import *
+from parse import compile
+import numpy as np
+import math
+import operator
 
+
+# actual
 cin = IStream("in")
+ROWS = 50
+face_offsets = [
+    (0, ROWS), 
+    (0, 2*ROWS),
+    (ROWS, ROWS),
+    (2*ROWS, 0),
+    (2*ROWS, ROWS),
+    (3*ROWS, 0)
+]
+
+# right, down, left, up
+transfer = [
+    [1, 2, 3, 5],
+    [4, 2, 0, 5],
+    [1, 4, 3, 0],
+    [4, 5, 0, 2],
+    [1, 5, 3, 2],
+    [4, 1, 0, 3]
+]
+
+"""
+# example
+cin = IStream("sample_in")
+ROWS = 4
+face_offsets = [
+    (0, 2 * ROWS),
+    (ROWS, 0),
+    (ROWS, ROWS),
+    (ROWS, 2 * ROWS),
+    (2 * ROWS, 2 * ROWS),
+    (2 * ROWS, 3 * ROWS)
+]
+
+transfer = [
+    [5, 3, 2, 1],
+    [2, 4, 5, 0],
+    [3, 4, 1, 0],
+    [5, 4, 2, 0],
+    [5, 1, 2, 3],
+    [0, 1, 4, 3]
+]
+"""
+max_len = 0
+maze_input = []
+
+lines = cin.all_lines()
+for line in lines[:-2]:
+    if max_len == 0:
+        maze_input.append(line)
+    else:
+        maze_input.append("".join(padded(line, " ", max_len)))
+    if len(line) > max_len:
+        max_len = len(line)
+        maze_input = ["".join(padded(item, " ", max_len)) for item in maze_input]
+
+
+faces = [[row[o_c:o_c + ROWS] for row in maze_input[o_r:o_r + ROWS]] for o_r, o_c in face_offsets]
+
+
+space_face = []
+walls_face = []
+
+for face in faces:
+    space = set()
+    walls = set()
+    for r, row in enumerate(face):
+        for c, ele in enumerate(row):
+            if ele == ".":
+                space.add((r, c))
+            if ele == "#":
+                walls.add((r, c))
+    space_face.append(space)
+    walls_face.append(walls)
+
+cw = {
+    (0, 1): (1, 0),
+    (1, 0): (0, -1),
+    (0, -1): (-1, 0),
+    (-1, 0): (0, 1)
+}
+
+ccw = {
+    (0, 1): (-1, 0),
+    (-1, 0): (0, -1),
+    (0, -1): (1, 0),
+    (1, 0): (0, 1)
+}
+
+idx = {
+    (0, 1): 0,
+    (1, 0): 1,
+    (0, -1): 2,
+    (-1, 0): 3
+}
+
+from_idx = {
+    0: (0, -1),
+    1: (-1, 0),
+    2: (0, 1),
+    3: (1, 0)
+}
+
+transfer_dir = {
+    ((0, 1), (0, 1)): lambda t: (t[0], 0),
+    ((0, 1), (1, 0)): lambda t: (0, (ROWS - 1) - t[0]),
+    ((0, 1), (0, -1)): lambda t: ((ROWS - 1) - t[0], (ROWS - 1)),
+    ((0, 1), (-1, 0)): lambda t: ((ROWS - 1), t[0]),
+    ((1, 0), (0, 1)): lambda t: ((ROWS - 1) - t[1], 0),
+    ((1, 0), (1, 0)): lambda t: (0, t[1]),
+    ((1, 0), (0, -1)): lambda t: (t[1], (ROWS - 1)),
+    ((1, 0), (-1, 0)): lambda t: ((ROWS - 1), (ROWS - 1) - t[1]),
+    ((0, -1), (0, 1)): lambda t: ((ROWS - 1) - t[0], 0),
+    ((0, -1), (1, 0)): lambda t: (0, t[0]),
+    ((0, -1), (0, -1)): lambda t: (t[0], (ROWS - 1)),
+    ((0, -1), (-1, 0)): lambda t: ((ROWS - 1), (ROWS - 1) - t[0]),
+    ((-1, 0), (0, 1)): lambda t: (t[1], 0),
+    ((-1, 0), (1, 0)): lambda t: (0, (ROWS - 1) - t[1]),
+    ((-1, 0), (0, -1)): lambda t: ((ROWS - 1) - t[1], (ROWS - 1)),
+    ((-1, 0), (-1, 0)): lambda t: ((ROWS - 1), t[1])
+}
+
+face = 0
+curr = (0, 0)
+direction = (0, 1)
+instructions = lines[-1]
+curr_instruction = ""
+for char in instructions:
+    if char.isalpha():
+        # Walk
+        for i in range(int(curr_instruction)):
+            next_spot = (curr[0] + direction[0], curr[1] + direction[1])
+            next_face = face
+            next_direction = direction
+            if (next_spot not in walls_face[face]) and (next_spot not in space_face[face]):
+                next_face = transfer[face][idx[direction]]
+                next_direction = from_idx[transfer[next_face].index(face)]
+                next_spot = transfer_dir[(direction, next_direction)](next_spot)
+                # print(face, next_face)
+                # print(direction, next_direction)
+                # print(curr, next_spot)
+            if next_spot in walls_face[next_face]:
+                break
+            curr = next_spot
+            face = next_face
+            direction = next_direction
+        # Turn
+        if char == "L":
+            direction = ccw[direction]
+        if char == "R":
+            direction = cw[direction]
+        curr_instruction = ""
+    else:
+        curr_instruction += char
+# Walk
+for i in range(int(curr_instruction)):
+    next_spot = (curr[0] + direction[0], curr[1] + direction[1])
+    next_face = face
+    next_direction = direction
+    if next_spot not in walls_face[face] and next_spot not in space_face[face]:
+        next_face = transfer[face][idx[direction]]
+        next_direction = from_idx[transfer[next_face].index(face)]
+        next_spot = transfer_dir[(direction, next_direction)](next_spot)
+        print(face, next_face)
+        print(direction, next_direction)
+        print(curr, next_spot)
+    if next_spot in walls_face[next_face]:
+        break
+    curr = next_spot
+    face = next_face
+    direction = next_direction
+
+curr = (curr[0] + face_offsets[face][0], curr[1] + face_offsets[face][1])
+print(1000 * (curr[0] + 1) + 4 * (curr[1] + 1) + idx[direction])
